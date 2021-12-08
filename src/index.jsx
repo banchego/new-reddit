c13f8525f59b3441badc32fa33375012d4873352
banchego <nbnikitanb@gmail.com>
2021-12-08 16:08:19 +0800

    second commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+window.addEventListener(type:'load', listener()=>{
+
+ReactDom.render(<Header />, document.getElementById(elementid:'react_root'))
+});
