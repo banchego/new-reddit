0a1632f9cbd5ea692e64d314b6ef60479068a257
banchego <nbnikitanb@gmail.com>
2021-12-20 01:52:52 +0800

    still can't load header

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { Header} from "./Header";
+import { Header } from './Header';
 
-window.addEventListener('load', () => {
-    ReactDOM.render(<Header />, document.getElementById('react_root'));
+
+window.addEventListener('load', () => { 
+ReactDOM.render(<Header />, document.getElementById('react_root'));
 });
